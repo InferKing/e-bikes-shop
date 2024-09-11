bfbc9cefd38ac92d5bc7a1366546ba877c96cfc6
1nfer.exe <InferKing@users.noreply.github.com>
2024-09-11 10:55:38 +0500

    add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from app.routes.index import bp as index_bp
 from app.routes.error import error_404
+from app.database import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('configuration.DevelopmentConfig')
     app.register_blueprint(index_bp)
-
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.register_error_handler(404, error_404)
     return app
