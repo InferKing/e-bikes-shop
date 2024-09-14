c62164a0899cfb55a5e10f50026490f5625abbe7
Oleg <54029296+InferKing@users.noreply.github.com>
2024-09-14 15:21:42 +0500

    fix issue with wrong links in base.html

--- app/routes/shop/shop.py
+++ app/routes/shop/shop.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from app.database import db, User
+from app.database import db, User, Product
 
 shop = Blueprint("shop", __name__, url_prefix='/shop')
 
@@ -11,14 +11,23 @@ def index():
 
 @shop.route("/<string:category>")
 def products(category):
-    return render_template("products.html")
+    data = db.session.query(Product).filter_by(category=category).all()
+    return render_template("products.html", products=data)
 
 @shop.route("/<string:category>/<int:product_id>")
 def product(category, product_id):
     return render_template("product.html")
 
-@shop.route("/z")
-def z():
-    users = db.session.query(User).all()
-    print(users)
-    return "lol"
+# @shop.route("/z")
+# def z():
+#     new_pr = Product()
+#     new_pr.name = "Super power 8500"
+#     new_pr.description = "Super mega bike for all of you"
+#     new_pr.price = 10000
+#     new_pr.img_path = "img/products/bike1.png"
+#     new_pr.color = "red"
+#     new_pr.availability = 3
+#     new_pr.category = "e-bikes"
+#     db.session.add(new_pr)
+#     db.session.commit()
+#     return "lol"
