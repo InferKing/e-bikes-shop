10da4b415801350665a5d367254310134d68b685
Oleg <54029296+InferKing@users.noreply.github.com>
2024-09-15 23:56:37 +0500

    add shop routes

--- app/routes/shop/shop.py
+++ app/routes/shop/shop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort, session, request, jsonify
 from app.database import db, User, Product
 
 shop = Blueprint("shop", __name__, url_prefix='/shop')
@@ -12,11 +12,32 @@ def index():
 @shop.route("/<string:category>")
 def products(category):
     data = db.session.query(Product).filter_by(category=category).all()
+    if len(data) == 0:
+        abort(404)
     return render_template("products.html", products=data)
 
 @shop.route("/<string:category>/<int:product_id>")
 def product(category, product_id):
-    return render_template("product.html")
+    product = db.session.query(Product).filter_by(id=product_id).first()
+    if product is None:
+        abort(404)
+    all_ids = find_id(session.get("picked", []), product.id)
+    if len(all_ids) > 0:
+        return render_template("product.html", product=product, is_picked=True)
+    return render_template("product.html", product=product, is_picked=False)
+
+@shop.route("/picked_product", methods=["POST"])
+def picked():
+    data = request.get_json()
+    r = session.get("picked")
+    if r is None:
+        session["picked"] = [data]
+    else:
+        session["picked"].append(data)
+    return jsonify({'status': 'ok'})
+
+def find_id(data, id):
+    return list(filter(lambda x: x["productId"] == id, data))
 
 # @shop.route("/z")
 # def z():
