febb5828923d5fbe7812751e6a1781e798597e90
1nfer.exe <InferKing@users.noreply.github.com>
2024-09-16 17:46:57 +0500

    update cart and checkout

--- app/routes/cart/cart.py
+++ app/routes/cart/cart.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.database import db, Product
 
-bp = Blueprint('cart', __name__, url_prefix='/cart')
 
-@bp.route('/')
-def index():
-    return render_template('cart.html')
+bp = Blueprint("cart", __name__, url_prefix="/cart")
+
+@bp.route("/")
+def index():    
+    if "picked" not in session:
+        return render_template("cart.html", error_msg="Cart is empty!")
+    temp_data = []
+    for kv in session["picked"]:
+        temp_data.append(db.session.query(Product).filter_by(id=kv.get("productId")).first())
+    return render_template("cart.html", data=temp_data)
--- app/routes/cart/checkout.py
+++ app/routes/cart/checkout.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 bp = Blueprint('checkout', __name__, url_prefix='/checkout')
 
 @bp.route('/', methods=["GET", "POST"])
 def index():
     return render_template('checkout.html')
+
+
+@bp.route("/billing", methods=["POST"])
+def billing():
+    return jsonify({"status": "OK"})
